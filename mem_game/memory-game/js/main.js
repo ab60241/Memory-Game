3b7342f8c22a90e6a4a19f933540b477d55b7cb6
Benckendorf Austin <BenckendorfAustin@JohnDeere.com>
2020-07-28 20:55:26 -0500

    created game logic

--- mem_game/memory-game/js/main.js
+++ mem_game/memory-game/js/main.js
@@ -1,6 +1,28 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+let cardOne = cards [0]
+cardsInPlay.push('cardOne')
+
+
+console.log("User flipped" + cardOne);
+
+let cardTwo = cards [2]
+cardsInPlay.push('cardTwo')
+
+console.log("User flipped" + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {							// if cards in play === 2 , need to check if cards match
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again");
+	}
+	
+// store once I figure this out Stage your code.
+//git add -A
+//Create a version of your staged code.
+//git commit -m 'created game logic'
+//Push your code to the GitHub repository.
+//git push origin master
+
